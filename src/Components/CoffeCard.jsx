08a0535b36f512294d244f30750fbe1971771e10
torikulislamv3 <tamim.officialinfo7@gmail.com>
2024-04-24 15:57:50 +0600

    delete method is added and sweet alert addede

--- src/Components/CoffeCard.jsx
+++ src/Components/CoffeCard.jsx
@@ -1,5 +1,49 @@
+import Swal from "sweetalert2";
+
 const CoffeCard = ({coffee}) => {
-    const {coffeeName, supplierName, quantity, Testy, category, details, url} = coffee;
+    const {_id ,coffeeName, supplierName, quantity, Testy, category, details, url} = coffee;
+
+    const hanldeDelete = _id =>{
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method : 'DELETE'
+                })
+                .then(res=> res.json())
+                .then(data => {
+                    console.log(data);
+                   if(data.deletedCount>0){
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                      });
+                   }
+                   else{
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Something went wrong!",
+                        footer: '<a href="#">Why do I have this issue?</a>'
+                      });
+                   }
+                })
+
+              
+            }
+          });
+    }
+
     return (
         <div>
             <div className="flex justify-evenly items-center border p-10 rounded-lg bg-slate-400">
@@ -19,7 +63,9 @@ const CoffeCard = ({coffee}) => {
                     </div>
                     <div className="mt-7">
                         <button className="btn btn-primary mr-5 text-[#FFFFFF]">Update</button>
-                        <button className="btn btn-ghost bg-red-900 mr-5 text-[#FFFFFF]">Delete</button>
+                        <button
+                        onClick={()=> hanldeDelete(_id)}
+                         className="btn btn-ghost bg-red-900 mr-5 text-[#FFFFFF]">Delete</button>
                         <button className="btn btn-accent text-[#FFFFFF] ">Details</button>
                     </div>
                 </div>
